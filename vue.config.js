acfee00cf98597cff267d965345d0e90f6e115b7
l0oktaan <looktaan.tc3@gmail.com>
2024-04-21 19:03:39 +0700

    test api proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  devServer: {
+    proxy: {
+      "^/api" :{
+        target: 'https://www.boredapi.com',
+        // changeOrigin: true,
+        // pathRewrite: {'^/api' : ''}
+        // target: 'http://localhost:8000'
+      },
+    }
+  }
+}
